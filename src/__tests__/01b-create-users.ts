9db9b1313fd7191ce1939849358b866f2a0191d7
Kartik Shah <krtk.6160@gmail.com>
2021-06-27 13:55:23 +0530

    Add 2fa checks and tests

--- src/__tests__/01b-create-users.ts
+++ src/__tests__/01b-create-users.ts
@@ -2,10 +2,14 @@
  * @jest-environment node
  */
 import mongoose from "mongoose"
+import { generateToken } from "node-2fa"
+import { AdminOps } from "../AdminOps"
 import { usernameExists } from "../AdminOps"
 import { yamlConfig } from "../config"
+import { baseLogger } from "../logger"
 import { setupMongoConnection } from "../mongodb"
 import { User } from "../schema"
+import { UserWallet } from "../userWallet"
 import { getUserWallet } from "./helper"
 
 jest.mock("../realtimePrice")
@@ -132,9 +136,32 @@ describe("username tests", () => {
     await expect(userWallet2.setUsername({ username })).rejects.toThrow()
   })
 
-  // FIXME: failing for some reason
-  // it('sets account status correctly', async () => {
-  //   await setAccountStatus({uid: userWallet2._id, status: 'locked'})
-  //   await expect(userWallet2.status).toBe('locked')
-  // })
+  it("sets account status correctly", async () => {
+    await AdminOps.setAccountStatus({ uid: userWallet2.user._id, status: "locked" })
+    userWallet2 = await getUserWallet(2)
+    await expect(userWallet2.user.status).toBe("locked")
+  })
+
+  it("set 2fa for user0", async () => {
+    const { secret } = userWallet0.generate2fa()
+    const token = generateToken(secret)!.token
+    await userWallet0.save2fa({ secret, token })
+
+    userWallet0 = await getUserWallet(0)
+    expect(userWallet0.user.twoFactor.secret).toBe(secret)
+  })
+
+  it("validate 2fa for user0", async () => {
+    const secret = userWallet0.user.twoFactor.secret
+    const token = generateToken(secret)?.token
+    expect(UserWallet.validate2fa({ token, logger: baseLogger, secret })).toBeTruthy()
+  })
+
+  it("delete 2fa for user0", async () => {
+    const token = generateToken(userWallet0.user.twoFactor.secret)?.token
+    const result = await userWallet0.delete2fa({ token })
+    expect(result).toBeTruthy()
+    userWallet0 = await getUserWallet(0)
+    expect(userWallet0.user.twoFactor.secret).toBeFalsy()
+  })
 })
